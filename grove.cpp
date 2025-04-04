439b87d34a4f838fdcdf579f1f3efa62e3031902
Juri Wolf <Amerlander@users.noreply.github.com>
2025-04-04 17:55:33 +0200

    Update isCodal function block ID and remove deprecated flag

--- grove.cpp
+++ grove.cpp
@@ -5,8 +5,9 @@ namespace grove {
      * Check if the runtime is Codal
      */
     //% block="Is Codal?"
+    //% blockId="grove_is_codal"
     //% group="Ultrasonic"
-    //% hidden=1 deprecated=1
+    //% hidden=1
     bool isCodal() {
 #if MICROBIT_CODAL
        return true;
